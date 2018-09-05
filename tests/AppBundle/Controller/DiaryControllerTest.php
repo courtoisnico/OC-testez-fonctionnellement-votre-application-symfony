b8742b75555d930265da5674eace5a1d2eb25f01
ncourtois <ncourtois@riu.fr>
2018-09-05 09:28:35 +0200

    Fin cours

--- tests/AppBundle/Controller/DiaryControllerTest.php
+++ tests/AppBundle/Controller/DiaryControllerTest.php
@@ -30,4 +30,59 @@ class DiaryControllerTest extends WebTestCase
         $this->assertSame(1, $crawler->filter('html:contains("Bienvenue sur FoodDiary !")')->count());
     }
 
+    public function testAddRecord()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/diary/add-new-record');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['food[username]'] = 'John Doe';
+        $form['food[entitled]'] = 'Plat de pâtes';
+        $form['food[calories]'] = 600;
+        $crawler = $client->submit($form);
+
+        $crawler = $client->followRedirect(); // Attention  à bien récupérer le crawler mis à jour
+
+//        echo $client->getResponse()->getContent();
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+
+    public function testAddRecordEmptyField()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/diary/add-new-record');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['food[username]'] = 'John Doe';
+        $form['food[entitled]'] = null;
+        $form['food[calories]'] = null;
+        $crawler = $client->submit($form);
+
+//        $client->followRedirect(); // Attention  à bien récupérer le crawler mis à jour
+
+//        echo $client->getResponse()->getContent();
+//        var_dump($crawler->filter('div.has-error'));
+        $this->assertSame(2, $crawler->filter('div.has-error')->count());
+    }
+
+    public function testList()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->selectLink('Voir tous les rapports')->link();
+        $crawler = $client->click($link);
+
+        $info = $crawler->filter('h1')->text();
+        $info = $string = trim(preg_replace('/\s\s+/', ' ', $info)); // On retire les retours à la ligne pour faciliter la vérification
+
+//        echo $client->getResponse()->getContent();
+//        echo $info;
+
+        $this->assertSame("Tous les rapports Tout ce qui a été mangé !", $info);
+    }
+
 }
